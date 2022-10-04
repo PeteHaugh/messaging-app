08af3a1e17d669f5645d808a66b86600e906cd9d
Peter Haugh <peter.ahaugh@gmail.com>
2022-10-04 19:13:37 +0100

    Added top bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className='app'>
-      <h1>Lets build a messenger</h1>
+    <div className="app">
+      <Routes>
+        <>
+        <Route index element={<Header />} />
+          
+          <Route path="/" element={<></>}></Route>
+        </>
+      </Routes>
     </div>
   );
 }
