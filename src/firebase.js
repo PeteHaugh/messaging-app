53e8fc2baaf05ca79ae5807dd3ce76d725d0c756
Peter Haugh <peter.ahaugh@gmail.com>
2022-10-04 18:19:55 +0100

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBVwSlh-ziaXeCQsLIWV8cGJl104VbYBV0",
+    authDomain: "messaging-app-cf251.firebaseapp.com",
+    projectId: "messaging-app-cf251",
+    storageBucket: "messaging-app-cf251.appspot.com",
+    messagingSenderId: "344853546528",
+    appId: "1:344853546528:web:b789eaddd19b1de5b55e45",
+    measurementId: "G-Q21E5MCPBG"
+  };
