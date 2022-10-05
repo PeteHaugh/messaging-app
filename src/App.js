b53972544c113232f2b2565ea28384788a7c5679
Peter Haugh <peter.ahaugh@gmail.com>
2022-10-05 21:50:00 +0100

    Added chat component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import AppLayout from "./components/AppLayout";
+import Chat from "./components/Chat"
 
 function App() {
   return (
     <div className="app">
       <Routes>
         <Route element={<AppLayout/>}>
-          <Route path="/" exact element={<></>}></Route>
+          <Route path="/" exact element={<Chat/>}></Route>
         </Route>
       </Routes>
     </div>
